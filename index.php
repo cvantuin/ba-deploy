e30eae70acbb8f94e7c78a4a349dfae9c369303e
cvantuin <cvantuin@redhat.com>
2016-11-08 07:27:19 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
  <?php 
- #echo "I am VERSION 6  ";
- $AUTHOR = getenv('DEMO_TEXT') ;
- echo "V2 by $AUTHOR " ;
+ echo "I am VERSION 1  ";
+ #$AUTHOR = getenv('DEMO_TEXT') ;
+ #echo "V2 by $AUTHOR " ;
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
  ?>
