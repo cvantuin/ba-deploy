a448f908ae23c2d2d52f2fec945bf51bb4aa48be
cvantuin <cvantuin@redhat.com>
2016-12-22 12:50:23 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
  <?php 
  #echo "I  am VERSION  7  " ;
  $AUTHOR = getenv('DEMO_TEXT') ;
- echo "V8 by $AUTHOR      " ;
+ echo "V9 by $AUTHOR      " ;
  echo "Pod IP: ".$_SERVER['SERVER_ADDR'] ;
  ?>
