31dc906b8ed6312de9cd833f833ee978ead05820
cvantuin <cvantuin@redhat.com>
2018-04-03 16:07:20 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
  # echo "I  am VERSION 1  " ;
 # test
  $AUTHOR = getenv('DEMO_TEXT') ;
- echo "app-b V1 by $AUTHOR      " ;
+ echo "app-b V2 by $AUTHOR      " ;
  echo "Pod IP: ".$_SERVER['SERVER_ADDR'] ;
  ?>
