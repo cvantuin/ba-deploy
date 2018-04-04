0caebf094c6406295e04daed7f9d5268c4dc8352
cvantuin <cvantuin@redhat.com>
2018-04-03 18:50:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,8 @@
  $AUTHOR = getenv('DEMO_TEXT') ;
  echo "app-a V2 by $AUTHOR   " ;
  echo "Pod IP: ".$_SERVER['SERVER_ADDR'] ;
+ $fh = fopen($myFile, 'r');
+ $theData = fread($fh, 1);
+ fclose($fh);
+ echo $theData;
  ?>
