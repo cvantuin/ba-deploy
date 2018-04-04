d85f894d143040ec4d532900cc0420098a87cce2
cvantuin <cvantuin@redhat.com>
2018-04-03 19:05:33 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
  $AUTHOR = getenv('DEMO_TEXT') ;
  echo "app-a V2 by $AUTHOR   " ;
  echo "Pod IP: ".$_SERVER['SERVER_ADDR'] ;
- echo file_get_contents( "/data/myconfigmap" )
-
+$configmap = file_get_contents('/data/DEMO_TEXT');
+echo $configmap;
  ?>
