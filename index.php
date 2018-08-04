d2c18d00e8a473f058d28ff527fad07b498d0051
Chris Van Tuin <cvantuin@blackoak.local>
2018-08-04 10:32:40 -0700

    v3

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
  # echo "I  am VERSION 10  " ;
 # test
  $AUTHOR = getenv('DEMO_TEXT') ;
- echo "V1 by $AUTHOR      " ;
+ echo "V3 by $AUTHOR      " ;
  echo "Pod IP: ".$_SERVER['SERVER_ADDR'] ;
  ?>
