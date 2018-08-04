5f86df15386760ad97ba2489e1c9ce1526e7df95
Chris Van Tuin <cvantuin@blackoak.local>
2018-08-04 15:39:08 -0700

    v1

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
  <?php 
- $AUTHOR = getenv('DEMO_TEXT') ;
- echo "V1 by $AUTHOR      " ;
- echo "Pod IP: ".$_SERVER['SERVER_ADDR'];
- echo "\n\32";
+$dbname="demodb";
+$dbuser="developer";
+$dbpass="developer";
+$dbhost="mariadb-101-centos7";
+
+$version = 1;
+$author = "unknown";
+$author  = getenv('DEMO_TEXT') ;
+
+$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
+$test_query = "select email FROM email where `id` = $version";
+$result = mysqli_query($link, $test_query);
+$row = mysqli_fetch_assoc($result);
+
+echo "v$version ";
+echo "   pod: ".$_SERVER['SERVER_ADDR'];
+echo "   email: ";
+echo $row['email'];
+echo "   user: $AUTHOR";
+echo "\n\32";
+
  ?>
