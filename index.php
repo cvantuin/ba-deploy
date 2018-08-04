e04563d0616f5361a295b1b0204b852e3d631d4d
Chris Van Tuin <cvantuin@blackoak.local>
2018-08-04 16:00:54 -0700

    v1

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $dbpass="developer";
 $dbhost="mariadb-101-centos7";
 
 $version = 1;
-$author  = getenv('DEMO_TEXT') ;
+$author = getenv('DEMO_TEXT') ;
 $email = "unknown";
 
 $link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
@@ -13,12 +13,12 @@ mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
 $test_query = "select email FROM email where `id` = $version";
 $result = mysqli_query($link, $test_query);
 $row = mysqli_fetch_assoc($result);
-$EMAIL = $row['email'];
+$email = $row['email'];
 
 echo "v$version ";
-echo "   pod: ".$_SERVER['SERVER_ADDR'];
-echo "   email: $EMAIL ";
-echo "   user: $AUTHOR";
+echo "\t pod: ".$_SERVER['SERVER_ADDR'];
+echo "\t email: $email"; 
+echo "\t user: $author";
 echo "\n\32";
 
  ?>
