19b86c845d0613686772ea15f144df44c283bc25
cvantuin <cvantuin@redhat.com>
2019-07-28 06:08:44 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,9 @@
  <?php 
-$dbname="demodb";
-$dbuser="developer";
-$dbpass="developer";
-$dbhost="mariadb";
-
-$version = 1;
-$author = getenv('DEMO_TEXT') ;
-$email = "unknown";
-
-$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
-$test_query = "select email FROM email where `id` = $version";
-$result = mysqli_query($link, $test_query);
-$row = mysqli_fetch_assoc($result);
-$email = $row['email'];
-
-echo "v$version ";
-echo "\t pod: ".$_SERVER['SERVER_ADDR'];
-echo "\t email: $email"; 
-echo "\t user: $author";
-echo "\n\32";
-
+ #    echo "  " ;
+#        test
+ $AUTHOR = getenv('DEMO_TEXT') ;
+ echo "app-a V2 by $AUTHOR   " ;
+ echo "Pod IP: ".$_SERVER['SERVER_ADDR'] ;
+$configmap = file_get_contents('/data/DEMO_TEXT');
+echo " $configmap";
  ?>
