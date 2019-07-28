c0d1d8505f5733937c43acfa21f0a5652d5aa4bb
Cloud User <ec2-user@ip-172-31-30-240.us-west-2.compute.internal>
2019-07-28 17:47:51 +0000

    v3

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
  <?php 
  $AUTHOR = getenv('DEMO_TEXT') ;
- echo "V1 by $AUTHOR   " ;
+ echo "V3 by $AUTHOR   " ;
  echo "Pod IP: ".$_SERVER['SERVER_ADDR'] ;
  # $configmap = file_get_contents('/data/DEMO_TEXT');
  # echo " $configmap";
