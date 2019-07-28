f9dcc9395ec24d7f438da2447b6aa7d8034954a9
Cloud User <cvantuin@redhat.com>
2019-07-28 18:18:44 +0000

    v2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
  <?php 
  $AUTHOR = getenv('DEMO_TEXT') ;
- echo "V4 by $AUTHOR   " ;
+ echo "V2 by $AUTHOR   " ;
  echo "Pod IP: ".$_SERVER['SERVER_ADDR'] ;
  # $configmap = file_get_contents('/data/DEMO_TEXT');
  # echo " $configmap";
